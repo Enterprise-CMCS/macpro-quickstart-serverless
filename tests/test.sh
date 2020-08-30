23ba13cd0b0ba413c3a8325d1e0ae69d3fcaa5b3
Mike Dial <mdial@collabralink.com>
2020-08-30 12:40:32 -0400

    use test.sh to run tests.

--- tests/test.sh
+++ tests/test.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-docker run --rm --network="data_net" --name test -e APPLICATION_ENDPOINT=http://react -v $(pwd)/testcafe:/tests testcafe/testcafe chromium /tests/**/*.js
+testcafe "chrome:headless" testcafe/**/*.js
