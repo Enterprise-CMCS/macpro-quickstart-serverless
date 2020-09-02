fa2d5f80f63f19454b8820192c552c16470bf71e
Mike Dial <mdial@collabralink.com>
2020-09-02 09:13:40 -0400

    refactor the ES output variables to make more sense

--- services/stream-functions/indexer.js
+++ services/stream-functions/indexer.js
@@ -1,10 +1,10 @@
 const { pushStream } = require('dynamodb-stream-elasticsearch');
 
-const { ES_ENDPOINT, INDEX, TYPE } = process.env;
+const { ES_ENDPOINT_URL, INDEX, TYPE } = process.env;
 
 function myHandler(event, context, callback) {
   console.log('Received event:', JSON.stringify(event, null, 2));
-  pushStream({ event, endpoint: ES_ENDPOINT, index: INDEX, type: TYPE })
+  pushStream({ event, endpoint: ES_ENDPOINT_URL, index: INDEX, type: TYPE })
     .then(() => {
       callback(null, `Successfully processed ${event.Records.length} records.`);
     })
