073ab853b454ef2dfebd2ea1b669f9c379ff3901
Mike Dial <mdial@collabralink.com>
2020-09-03 09:24:22 -0400

    Lay out a pattern for creating many mappings

--- services/elasticsearch-config/createMapping.js
+++ services/elasticsearch-config/createMapping.js
@@ -1,10 +1,10 @@
 
 import * as elasticsearch from "./libs/elasticsearch-lib";
-var mapping = require('./mappings/mapping.json');
 
-function myHandler(event, context, callback) {
+function amendmentsHandler(event, context, callback) {
+  var mapping = require('./mappings/amendments.json');
   console.log('Received event:', JSON.stringify(event, null, 2));
   elasticsearch.addMapping(mapping, process.env.INDEX, process.env.ES_ENDPOINT, 'us-east-1');
 }
 
-exports.handler = myHandler;
+exports.amendmentsHandler = amendmentsHandler;
