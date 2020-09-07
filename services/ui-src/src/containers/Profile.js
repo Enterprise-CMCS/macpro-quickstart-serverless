5b145eb2b7399073afe9a62e19d6a3983beaec12
Mike Dial <48921055+mdial89f@users.noreply.github.com>
2020-09-07 10:34:27 -0400

    Add Profile and ability to update user info (#48)
    * Functional profile updater addition
    
    * Convert profile/logout into a dropdown, and set the title to the user's user email
    
    * Add support to add a phone number.  Demonstrates the ability to add optional, new user attributes
    
    * Change phone number library and add validation
    
    Co-authored-by: Mike Dial <mdial@collabralink.com>

---
+++ services/ui-src/src/containers/Profile.js
@@ -0,0 +1,130 @@
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { onError } from "../libs/errorLib";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import LoaderButton from "../components/LoaderButton";
+import "./Profile.css";
+import { Auth } from "aws-amplify"
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
+
+export default function Profile() {
+    const history = useHistory();
+    const [email, setEmail] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const capitalize = (s) => {
+        if (typeof s !== 'string') return ''
+        return s.charAt(0).toUpperCase() + s.slice(1)
+    }
+
+    useEffect(() => {
+        function loadProfile() {
+            return Auth.currentUserInfo();
+        }
+
+        async function onLoad() {
+            try {
+                const userInfo = await loadProfile();
+                setEmail(userInfo.attributes.email);
+                setFirstName(capitalize(userInfo.attributes.given_name));
+                setLastName(capitalize(userInfo.attributes.family_name));
+                setPhoneNumber(formatPhoneNumberForForm(userInfo.attributes.phone_number));
+            } catch (e) {
+                onError(e);
+            }
+        }
+
+        onLoad();
+    }, []);
+
+    function validatePhoneNumber(phone) {
+        if ( phone === "1" || phone === "" ) return true;
+        return phone.length === 11;
+    }
+    function validateForm() {
+        return email.length > 0 && firstName.length > 0 && lastName.length && validatePhoneNumber(phoneNumber);
+    }
+
+    function saveProfile(user, userAttributes) {
+        return Auth.updateUserAttributes(user, userAttributes);
+    }
+
+    function formatPhoneNumberForForm(phone) {
+        if ( phone == null ) return ""
+        return phone.replace('+', '')
+    }
+
+    function formatPhoneNumberForSubmission(phone) {
+        if (phone === "1" || phone === "" || phone == null ) return ""
+        return "+" + phone.replace('+', '')
+    }
+    async function handleSubmit(event) {
+
+        event.preventDefault();
+        setIsLoading(true);
+        let user = await Auth.currentAuthenticatedUser();
+        try {
+            await saveProfile(user, {
+                "given_name": firstName,
+                "family_name": lastName,
+                "phone_number": formatPhoneNumberForSubmission(phoneNumber)
+            });
+            history.push("/");
+        } catch (e) {
+            onError(e);
+            setIsLoading(false);
+        }
+    }
+
+    return (
+        <div className="Profile">
+            <form onSubmit={handleSubmit}>
+                <FormGroup controlId="email">
+                    <ControlLabel>Email</ControlLabel>
+                    <FormControl
+                        value={email}
+                        disabled={true}
+                    />
+                </FormGroup>
+                <FormGroup controlId="firstName">
+                    <ControlLabel>First Name</ControlLabel>
+                    <FormControl
+                        value={firstName}
+                        onChange={e => setFirstName(e.target.value)}
+                    />
+                </FormGroup>
+                <FormGroup controlId="lastName">
+                    <ControlLabel>Last Name</ControlLabel>
+                    <FormControl
+                        value={lastName}
+                        onChange={e => setLastName(e.target.value)}
+                    />
+                </FormGroup>
+                <FormGroup controlId="phoneNumber">
+                    <ControlLabel>Phone</ControlLabel>
+                    <PhoneInput
+                        value={phoneNumber}
+                        country="us"
+                        countryCodeEditable={false}
+                        disableDropdown={true}
+                        enableAreaCodes={false}
+                        onChange={e => setPhoneNumber(e || "" )}
+                    />
+                </FormGroup>
+                <LoaderButton
+                    block
+                    type="submit"
+                    bsSize="large"
+                    bsStyle="primary"
+                    isLoading={isLoading}
+                    disabled={!validateForm()}
+                >
+                    Save
+                </LoaderButton>
+            </form>
+        </div>
+    );
+}
