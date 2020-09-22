e51b6f6d570d8023535a184cdc80be2196c2c1fb
Mike Dial <mdial@collabralink.com>
2020-09-22 13:15:50 -0400

    Add Cloudtamer support back in

--- .github/ctkey.sh
+++ .github/ctkey.sh
@@ -8,5 +8,5 @@ if [ -n "$CTKEY_USERNAME" ]; then
   unzip ctkey.zip -d ctkey
   mkdir ~/.aws
   ./ctkey/ctkey-linux savecreds --url=$CTKEY_URL --account=$CTKEY_ACCOUNT_ID --iam-role=$CTKEY_IAM_ROLE --idms=2
-  echo "export AWS_PROFILE=${CTKEY_ACCOUNT_ID}_${CTKEY_IAM_ROLE}" >> $BASH_ENV
+  "::set-env name=AWS_PROFILE::${CTKEY_ACCOUNT_ID}_${CTKEY_IAM_ROLE}"
 fi
