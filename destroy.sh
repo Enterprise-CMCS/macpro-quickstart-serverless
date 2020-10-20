96e83e7f74914f80adaf6ec9b8d9cb8ce755f88e
Mike Dial <mdial@collabralink.com>
2020-10-20 10:38:16 -0400

    Destroy via Cloudformation and the CLI.  This is more forceful but does a better job of cleaning up and is also not bound to the version of code that was deployed.

--- destroy.sh
+++ destroy.sh
@@ -1,40 +1,64 @@
 #!/bin/bash
-
 set -e
 
-stage=${1:-dev}
+if [[ $1 == "" ]] ; then
+    echo 'ERROR:  You must pass a stage to destroy.  Ex. sh destroy.sh my-stage-name'
+    exit 1
+fi
+stage=$1
 
-# Clean up existing, non-empty buckets
-pushd services
-ui_bucket_name=`./output.sh ui S3BucketName $stage`
-aws s3 rm s3://$ui_bucket_name --recursive
-uploads_bucket_name=`./output.sh uploads AttachmentsBucketName $stage`
-aws s3 rm s3://$uploads_bucket_name --recursive
-popd
+echo "\nCollecting information on stage $stage before attempting a destroy... This can take a minute or two..."
+# Find buckets associated with stage
+bucketList=(`aws s3api list-buckets --output text --query 'Buckets[*].Name'` )
+filteredBucketList=()
+for i in "${bucketList[@]}"
+do
+  stage_tag=`aws s3api get-bucket-tagging --bucket $i --output text --query 'TagSet[?Key==\`STAGE\`].Value'`
+  if [ "$stage_tag" == "$stage" ]; then
+    filteredBucketList+=($i)
+  fi
+done
 
-services=(
-  'database'
-  'uploads'
-  'uploads-scan'
-  'app-api'
-  'elasticsearch-auth'
-  'elasticsearch'
-  'stream-functions'
-  'ui-auth'
-  'ui'
-  # Running remove on ui-src would delete the s3 bucket and cause remove on ui to fail.
-  # We empty the bucket near the top of this file, and allow ui to delete it
-  # 'ui-src'
-)
+# Find cloudformation stacks associated with stage
+filteredStackList=(`aws cloudformation describe-stacks | jq -r ".Stacks[] | select(.Tags[] | select(.Key==\"STAGE\") | select(.Value==\"$stage\")) | .StackName"`)
 
-deploy() {
-  service=$1
-  pushd services/$service
-  yarn install
-  serverless remove --stage $stage
-  popd
-}
 
-for (( idx=${#services[@]}-1 ; idx>=0 ; idx-- )) ; do
-    deploy ${services[idx]}
+echo """
+********************************************************************************
+- Check the following carefully -
+********************************************************************************
+"""
+
+echo "The following buckets will be emptied"
+printf '%s\n' "${filteredBucketList[@]}"
+
+echo "\nThe following stacks will be destroyed:"
+printf '%s\n' "${filteredStackList[@]}"
+
+echo """
+********************************************************************************
+- Scroll up and check carefully -
+********************************************************************************
+"""
+
+read -p "Do you wish to continue?  Re-enter the stage name to continue:  " -r
+echo
+if [[ ! $REPLY == "$stage" ]]
+then
+    echo "Stage name not re-entered.  Doing nothing and exiting."
+    exit 1
+fi
+
+
+for i in "${filteredBucketList[@]}"
+do
+  echo $i
+  aws s3 rm s3://$i/ --recursive
+done
+
+
+for i in "${filteredStackList[@]}"
+do
+  echo $i
+  aws cloudformation delete-stack --stack-name $i
 done
