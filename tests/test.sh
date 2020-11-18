e934f47b44d8f86339ece892c88d5f456fa5b70b
Ishmeal Baawuah <ibaawuah@Ishmeals-MBP.fios-router.home>
2020-11-18 16:06:42 -0500

    adding first nightwatch test

--- tests/test.sh
+++ tests/test.sh
@@ -2,4 +2,5 @@
 
 set -e
 
-testcafe "chrome:headless" testcafe/**/*.js --reporter json:testcafe/testcafe_results/results.json
+#testcafe "chrome:headless" testcafe/**/*.js --reporter json:testcafe/testcafe_results/results.json
+npm test
