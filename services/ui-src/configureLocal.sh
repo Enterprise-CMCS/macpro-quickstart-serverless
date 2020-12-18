78cdbf6d4dcc5ec40565293cc41c5d2d1e286a13
Mike Dial <michael.dial.4@gmail.com>
2020-12-18 07:42:34 -0500

    Update configureLocal.sh to continue to function.  I added a banner to warn that this may be obsolete and to try to avoid confusion.

--- services/ui-src/configureLocal.sh
+++ services/ui-src/configureLocal.sh
@@ -1,7 +1,18 @@
 
 set -e
 
+echo """
+Please Note:
+  This script exists to enable a developer to run a react frontend locally while pointing to an AWS backend.
+  This is *not* the same as deploying the entire stack locally.
+  This script may be removed soon, as the much more powerful local workflow should make this osoblete.
+  However, it will be left for now.
+  Please make sure you understand what this script does and why you're using it.
+"""
+
 stage=${1:-dev}
+
+echo "Fetching information for stage $stage..."
 
 api_region=`sh ../output.sh ../app-api Region $stage`
 api_url=`sh ../output.sh ../app-api ApiGatewayRestApiUrl $stage`
@@ -20,5 +31,7 @@ export COGNITO_USER_POOL_ID=$cognito_user_pool_id
 export COGNITO_CLIENT_ID=$cognito_client_id
 export S3_ATTACHMENTS_BUCKET_REGION=$s3_attachments_bucket_region
 export S3_ATTACHMENTS_BUCKET_NAME=$s3_attachements_bucket_name
+# This is set to false, as using this script points your local react server to Amazon
+export LOCAL_LOGIN=false
 
 ./env.sh
