9adc53005467ad127504587228a9b77989052a6a
Mike Dial <michael.dial.4@gmail.com>
2021-02-16 16:53:15 -0500

    get clamav working with greenfield... the bucket policy is still not right... i think its bercause of the iam path

--- services/uploads/antivirus.js
+++ services/uploads/antivirus.js
@@ -17,6 +17,15 @@ const constants = require('./constants');
  * @return {int} Length of S3 object in bytes.
  */
 async function sizeOf(key, bucket) {
+    console.log("key: "+key);
+    console.log("bucket: "+bucket);
+    var sts = new AWS.STS();
+    var params = {
+ };
+ sts.getCallerIdentity(params, function(err, data) {
+   if (err) console.log(err, err.stack); // an error occurred
+   else     console.log(data);           // successful response
+ });
     let res = await s3.headObject({ Key: key, Bucket: bucket }).promise();
     return res.ContentLength;
 }
