5041329bb5f5d7b7a6e9efaa93c7fe5d57010f03
Mike Dial <michael.dial.4@gmail.com>
2021-02-17 10:52:12 -0500

    correctly remove debug

--- services/uploads/antivirus.js
+++ services/uploads/antivirus.js
@@ -22,10 +22,7 @@ async function sizeOf(key, bucket) {
     var sts = new AWS.STS();
     var params = {
  };
- sts.getCallerIdentity(params, function(err, data) {
-   if (err) console.log(err, err.stack); // an error occurred
-   else     console.log(data);           // successful response
- });
+
     let res = await s3.headObject({ Key: key, Bucket: bucket }).promise();
     return res.ContentLength;
 }
