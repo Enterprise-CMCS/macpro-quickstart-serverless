86a894209f28a35f12736cd85b57b28dea69f28e
Ishmeal Baawuah <ibaawuah@ip-10-232-37-161.ec2.internal>
2021-04-07 12:37:43 -0400

    using quote tables

--- services/apollo-lambda/graphql.js
+++ services/apollo-lambda/graphql.js
@@ -2,63 +2,41 @@ const { ApolloServer, gql } = require('apollo-server-lambda');
 const { unmarshall } = require("@aws-sdk/util-dynamodb");
 const { DynamoDBClient, ScanCommand } = require("@aws-sdk/client-dynamodb");
 const client = new DynamoDBClient({ region: "us-east-1" });
-const getAllAmendments = async () => {
+const getQuotes = async () => {
   const params = {
-    TableName: process.env.tableName,
+    TableName: "Quote",
   };
   try {
     const results = await client.send(new ScanCommand(params));
-    const amendments = [];
+    const quotes = [];
     results.Items.forEach((item) => {
-      amendments.push(unmarshall(item));
+      quotes.push(unmarshall(item));
     });
-    return amendments;
+    return quotes;
   } catch (err) {
     console.error(err);
     return err;
   }
 };
 // Construct a schema, using GraphQL schema language
-
 const typeDefs = gql`
-scalar JSON
-type Amendment{
-  amendmentId:  ID!
-  authProvider: String
-  comments:     String
-  email:        string
-}
-type User {
-  userId:    ID!
-  firstName: String!
-  lastName:  String!
-  email:     String!
-}
-
-type Query {
-  amendments: [Amendment]!
-  me: User
-}
-
+  scalar JSON
+  type Quote {
+    id: ID!
+    value: String
+    source: String
+  }
+  type Query {
+    quotes: [Quote]
+  }
 `;
-// Provide resolver functions for your schema fields
-//const resolvers = {
-//  Query: {
-//    amendments: (_, __, { dataSources }) =>
-//      dataSources.amendmentAPI.getAllAmendments(),
-//    me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
-//  }
-
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    amendments: () => {
-      return getAllAmendments();
+    quotes: () => {
+      return getQuotes();
     },
   },
-};
-
-
 };
 const server = new ApolloServer({
   typeDefs,
