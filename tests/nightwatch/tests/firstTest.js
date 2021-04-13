4589a2c288a334b8951f601a6368df72f10f9767
Ayo A <ayoneeyee@gmail.com>
2021-04-13 18:25:27 -0400

    Re-added original content 'APS Home' into the firstTest.js for nighatch code testing

--- tests/nightwatch/tests/firstTest.js
+++ tests/nightwatch/tests/firstTest.js
@@ -3,7 +3,7 @@ module.exports = {
     browser
       .url(`${process.env.APPLICATION_ENDPOINT}`)
       .waitForElementVisible(".navbar-brand")
-      .assert.containsText(".navbar-brand", "TEST ME")
+      .assert.containsText(".navbar-brand", "APS Home")
       .saveScreenshot("tests_output/My_first_test_case_screenshot.png");
   },
 };
