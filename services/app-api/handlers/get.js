3803f040d6efd907bca464feef86eb5dbe91be8a
Mike Dial <michael.dial.4@gmail.com>
2021-04-16 09:04:10 -0400

    Remove identical comments that dont add much

--- services/app-api/handlers/get.js
+++ services/app-api/handlers/get.js
@@ -10,9 +10,6 @@ export const main = handler(async (event, context) => {
 
   const params = {
     TableName: process.env.tableName,
-    // 'Key' defines the partition key and sort key of the item to be retrieved
-    // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'amendmentId': path parameter
     Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
       amendmentId: event.pathParameters.id,
