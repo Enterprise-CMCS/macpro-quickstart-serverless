3eb4f156e2ebb0af0576799b90a2319d123b8fb0
eolaniyan052 <eolaniyan@collabralink.com>
2021-04-21 11:09:37 -0400

    added extra workflow to validate branch name

---
+++ branchVal.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+local_branch=$1
+
+valid_branch_regex="^[a-z0-9_\-]+$"
+
+message="There is something wrong with your branch name. Branch names in this project must adhere to this contract: $valid_branch_regex. Your commit will be rejected. You should rename your branch to a valid name and try again."
+
+if [[ ! $local_branch =~ $valid_branch_regex ]]
+then
+    echo "$message"
+    exit 1
+fi
+
+exit 0
