9877e80afe11e4255a371136c8db56906ed073b6
eolaniyan052 <eolaniyan@collabralink.com>
2021-04-22 15:48:33 -0400

    fix regex

--- branchVal.sh
+++ branchVal.sh
@@ -4,7 +4,9 @@ set -e
 
 local_branch=${1}
 
-valid_branch_regex="^[a-z]+$"
+valid_branch_regex="^[^A-Z\s]+\.[^A-Z\s]+$"
+
+
 
 message="There is something wrong with your branch name. Branch names in this project must adhere to this contract: $valid_branch_regex, $local_branch. Your commit will be rejected. You should rename your branch to a valid name and try again."
 
