90d0135e116de5f27baf757756065b6c5d2fc870
Mike Dial <michael.dial.4@gmail.com>
2021-05-06 11:54:24 -0400

    ccfixes hopefully

--- services/stream-functions/handlers/emailSubmitter.js
+++ services/stream-functions/handlers/emailSubmitter.js
@@ -6,30 +6,11 @@ exports.handler = function (event, context, callback) {
     var params = (function (eventName) {
       switch (eventName) {
         case "INSERT":
-          return insertParams(record);
-        case "MODIFY":
-          return modifyParams(record);
-        case "REMOVE":
-          return removeParams(record);
-        default:
-          return 30;
-      }
-    })(record.eventName);
-
-    ses.sendEmail(params);
-  });
-  callback(null, "message");
-};
-
-function insertParams(record) {
-  return {
-    Destination: {
-      ToAddresses: [record.dynamodb.NewImage.email.S],
-    },
-    Message: {
-      Body: {
-        Text: {
-          Data: `
+          return ses.getSESEmailParams({
+            ToAddresses: [record.dynamodb.NewImage.email.S],
+            fromAddressSource: process.env.emailSource,
+            Subject: `New ACME APS submission received! - ${record.dynamodb.NewImage.transmittalNumber.S}`,
+            HTML: `
 Hi ${record.dynamodb.NewImage.firstName.S},
 
 We are writing to let you know we've received your Amendment to Planned Settlement (APS) submission!
@@ -43,79 +24,56 @@ Thank you for using our APS submission system!
 Regards,
 APS Team
 
-`,
-        },
-      },
-      Subject: {
-        Data: `New ACME APS submission received! - ${record.dynamodb.NewImage.transmittalNumber.S}`,
-      },
-    },
-    Source: process.env.emailSource,
-  };
-}
+`
+          });
+        case "MODIFY":
+          return ses.getSESEmailParams({
+            ToAddresses: [record.dynamodb.NewImage.email.S],
+            fromAddressSource: process.env.emailSource,
+            Subject: `Updated ACME APS submission received! - ${record.dynamodb.NewImage.transmittalNumber.S}`,
+            HTML: `
+  Hi ${record.dynamodb.NewImage.firstName.S},
 
-function modifyParams(record) {
-  return {
-    Destination: {
-      ToAddresses: [record.dynamodb.NewImage.email.S],
-    },
-    Message: {
-      Body: {
-        Text: {
-          Data: `
-Hi ${record.dynamodb.NewImage.firstName.S},
+  We are writing to let you know we've received an update to your Amendment to Planned Settlement (APS) submission!
+  It is under review.
+  No additional action is needed on your part.
 
-We are writing to let you know we've received an update to your Amendment to Planned Settlement (APS) submission!
-It is under review.
-No additional action is needed on your part.
+  APS ID: ${record.dynamodb.NewImage.transmittalNumber.S}
 
-APS ID: ${record.dynamodb.NewImage.transmittalNumber.S}
+  Thank you for using our APS submission system!
 
-Thank you for using our APS submission system!
+  Regards,
+  APS Team
 
-Regards,
-APS Team
+  `
+          });
+        case "REMOVE":
+          return ses.getSESEmailParams({
+            ToAddresses: [record.dynamodb.OldImage.email.S],
+            fromAddressSource: process.env.emailSource,
+            Subject: `Your ACME APS submission has been deleted - ${record.dynamodb.OldImage.transmittalNumber.S}`,
+            HTML: `
+  Hi ${record.dynamodb.OldImage.firstName.S},
 
-`,
-        },
-      },
-      Subject: {
-        Data: `Updated ACME APS submission received! - ${record.dynamodb.NewImage.transmittalNumber.S}`,
-      },
-    },
-    Source: process.env.emailSource,
-  };
-}
+  We received a request to delete your Amendment to Planned Settlement (APS) submission.
+  We are writing to let you know that we have processed that request.
+  No additional action is needed on your part.
 
-function removeParams(record) {
-  return {
-    Destination: {
-      ToAddresses: [record.dynamodb.OldImage.email.S],
-    },
-    Message: {
-      Body: {
-        Text: {
-          Data: `
-Hi ${record.dynamodb.OldImage.firstName.S},
+  APS ID: ${record.dynamodb.OldImage.transmittalNumber.S}
 
-We received a request to delete your Amendment to Planned Settlement (APS) submission.
-We are writing to let you know that we have processed that request.
-No additional action is needed on your part.
+  Thank you for using our APS submission system!
 
-APS ID: ${record.dynamodb.OldImage.transmittalNumber.S}
+  Regards,
+  APS Team
 
-Thank you for using our APS submission system!
+  `
+          });
+        default:
+          return 30;
+      }
+    })(record.eventName);
 
-Regards,
-APS Team
-
-`,
-        },
-      },
-      Subject: {
-        Data: `Your ACME APS submission has been deleted - ${record.dynamodb.OldImage.transmittalNumber.S}`,
-      },
-    },
-    Source: process.env.emailSource,
-  };
-}
+    ses.sendEmail(params);
+  });
+  callback(null, "message");
+};
