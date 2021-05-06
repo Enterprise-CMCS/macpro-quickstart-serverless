fe353acdf8013cc6d70c12dcd8d55d09e8b23d2e
dwhitecl <dwhite@collabralink.com>
2021-05-06 08:07:25 -0400

    checking in latest

--- services/ui-auth/handlers/createUsers.js
+++ services/ui-auth/handlers/createUsers.js
@@ -1,7 +1,7 @@
 var aws = require("aws-sdk");
 const COGNITO_CLIENT = new aws.CognitoIdentityServiceProvider({
   apiVersion: "2016-04-19",
-  region: "us-east-1"
+  region: "us-east-1",
 });
 const userPoolId = process.env.userPoolId;
 
@@ -10,48 +10,55 @@ async function myHandler(event, context, callback) {
   console.log(userPoolId);
   const users = [
     {
-      username: "alice",
+      username: "alice@example.com",
       attributes: [
         {
           Name: "email",
-          Value: "alice@example.com"
+          Value: "alice@example.com",
         },
         {
           Name: "given_name",
-          Value: "Alice"
+          Value: "Alice",
         },
         {
           Name: "family_name",
-          Value: "Foo"
+          Value: "Foo",
         },
         {
           Name: "email_verified",
-          Value: "true"
-        }
-      ]
+          Value: "true",
+        },
+      ],
     },
   ];
 
   for (var i = 0; i < users.length; i++) {
     console.log(users[i]);
     var poolData = {
-        UserPoolId: userPoolId,
-        Username: users[i].username,
-        DesiredDeliveryMediums: ["EMAIL"],
-        TemporaryPassword: "Abc@321",
-        UserAttributes: users[i].attributes
-      };
-      await COGNITO_CLIENT.adminCreateUser(poolData, (error, data) => {
-        console.log(error);
-        console.log(data);
-        callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(data)
-        });
-      });
+      UserPoolId: userPoolId,
+      Username: users[i].username,
+      DesiredDeliveryMediums: ["EMAIL"],
+      TemporaryPassword: "Abc@321456!",
+      UserAttributes: users[i].attributes,
+    };
+    await createUser(poolData);
   }
 }
 
-
+async function createUser(params) {
+  await new Promise((resolve, reject) => {
+    COGNITO_CLIENT.adminCreateUser(params, function (err, data) {
+      if (err) {
+        console.log("FAILED ", err, err.stack); // an error occurred
+        response = { statusCode: 500, body: { message: "FAILED", error: err } };
+        reject();
+      } else {
+        console.log("SUCCESS", data); // successful response
+        response = { statusCode: 200, body: { message: "SUCCESS" } };
+        resolve();
+      }
+    });
+  });
+}
 
 exports.handler = myHandler;
