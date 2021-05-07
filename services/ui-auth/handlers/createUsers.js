00bbcfbcd49d85432e023f1ad7ffeb40b7c2bf86
dwhitecl <dwhite@collabralink.com>
2021-05-07 09:36:46 -0400

    adding updating user attributes

--- services/ui-auth/handlers/createUsers.js
+++ services/ui-auth/handlers/createUsers.js
@@ -10,19 +10,19 @@ async function myHandler(event, context, callback) {
   console.log(userPoolId);
   const users = [
     {
-      username: "billy@example.com",
+      username: "paul@example.com",
       attributes: [
         {
           Name: "email",
-          Value: "billy@example.com",
+          Value: "paul@example.com",
         },
         {
           Name: "given_name",
-          Value: "Billy",
+          Value: "Paul",
         },
         {
           Name: "family_name",
-          Value: "Bob",
+          Value: "McCartney",
         },
         {
           Name: "email_verified",
@@ -31,19 +31,40 @@ async function myHandler(event, context, callback) {
       ],
     },
     {
-      username: "alice@example.com",
+      username: "john@example.com",
       attributes: [
         {
           Name: "email",
-          Value: "alice@example.com",
+          Value: "john@example.com",
         },
         {
           Name: "given_name",
-          Value: "Alice",
+          Value: "John",
         },
         {
           Name: "family_name",
-          Value: "Cooper",
+          Value: "Lennon",
+        },
+        {
+          Name: "email_verified",
+          Value: "true",
+        },
+      ],
+    },
+    {
+      username: "george@example.com",
+      attributes: [
+        {
+          Name: "email",
+          Value: "george@example.com",
+        },
+        {
+          Name: "given_name",
+          Value: "George",
+        },
+        {
+          Name: "family_name",
+          Value: "Harrison",
         },
         {
           Name: "email_verified",
@@ -60,29 +81,34 @@ async function myHandler(event, context, callback) {
       Username: users[i].username,
       DesiredDeliveryMediums: ["EMAIL"],
       TemporaryPassword: "Abc@321456!",
-      UserAttributes: users[i].attributes,
+      UserAttributes: users[i].attributes
     };
     var passwordData = {
-      Password: "BoatThatBass!!",
+      Password: process.env.bootstrapUsersPassword,
       UserPoolId: userPoolId,
       Username: users[i].username,
       Permanent: true
     };
+    var attributeData = {
+      Username: users[i].username,
+      UserPoolId: userPoolId,
+      UserAttributes: users[i].attributes
+    };
+
     await createUser(poolData);
+    //userCreate must set a temp password first, calling setPassword to set the password configured in SSM for consistent dev login
     await setPassword(passwordData);
-  //}
+    //if user exists and attributes are updated in this file updateUserAttributes is needed to update the attributes
+    await updateUserAttributes(attributeData);
 }
 
 async function createUser(params) {
   await new Promise((resolve, reject) => {
     COGNITO_CLIENT.adminCreateUser(params, function (err, data) {
       if (err) {
-        // console.log("FAILED ", err, err.stack); // an error occurred
-        // response = { statusCode: 500, body: { message: "FAILED", error: err } };
-        // reject();
         console.log("FAILED ", err, err.stack); // an error occurred
         response = { statusCode: 500, body: { message: "FAILED", error: err } };
-        resolve();
+        resolve(); //if user already exists, we still continue and ignore
       } else {
         console.log("SUCCESS", data); // successful response
         response = { statusCode: 200, body: { message: "SUCCESS" } };
@@ -97,7 +123,7 @@ async function setPassword(params) {
     COGNITO_CLIENT.adminSetUserPassword(params, function (err, data) {
       if (err) {
         console.log("FAILED to update password", err, err.stack); // an error occurred
-        //response = { statusCode: 500, body: { message: "FAILED", error: err } };
+        response = { statusCode: 500, body: { message: "FAILED", error: err } };
         reject();
       } else {
         console.log("SUCCESS", data);
@@ -106,6 +132,22 @@ async function setPassword(params) {
     });
   });
 }
+
+async function updateUserAttributes(params) {
+  await new Promise((resolve, reject) => {
+    COGNITO_CLIENT.adminUpdateUserAttributes(params, function (err, data) {
+      if (err) {
+        console.log("FAILED to update user attributes", err, err.stack); // an error occurred
+        response = { statusCode: 500, body: { message: "FAILED", error: err } };
+        reject();
+      } else {
+        console.log("SUCCESS", data);
+        resolve();
+      }
+    });
+  });
+}
+
 }
 
 exports.handler = myHandler;
