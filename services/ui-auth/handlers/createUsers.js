fa1308968caf8b1d8557c1233103c3c9fd2b86d8
dwhitecl <dwhite@collabralink.com>
2021-05-07 13:40:01 -0400

    removing comments and cleaning up a bit

--- services/ui-auth/handlers/createUsers.js
+++ services/ui-auth/handlers/createUsers.js
@@ -6,73 +6,6 @@ async function myHandler(event, context, callback) {
   console.log("USER POOL ID: ");
   console.log(userPoolId);
 
-
-  // const users = [
-  //   {
-  //     username: "paul@example.com",
-  //     attributes: [
-  //       {
-  //         Name: "email",
-  //         Value: "paul@example.com",
-  //       },
-  //       {
-  //         Name: "given_name",
-  //         Value: "Paul",
-  //       },
-  //       {
-  //         Name: "family_name",
-  //         Value: "McCartney",
-  //       },
-  //       {
-  //         Name: "email_verified",
-  //         Value: "true",
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     username: "john@example.com",
-  //     attributes: [
-  //       {
-  //         Name: "email",
-  //         Value: "john@example.com",
-  //       },
-  //       {
-  //         Name: "given_name",
-  //         Value: "John",
-  //       },
-  //       {
-  //         Name: "family_name",
-  //         Value: "Lennon",
-  //       },
-  //       {
-  //         Name: "email_verified",
-  //         Value: "true",
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     username: "george@example.com",
-  //     attributes: [
-  //       {
-  //         Name: "email",
-  //         Value: "george@example.com",
-  //       },
-  //       {
-  //         Name: "given_name",
-  //         Value: "George",
-  //       },
-  //       {
-  //         Name: "family_name",
-  //         Value: "Harrison",
-  //       },
-  //       {
-  //         Name: "email_verified",
-  //         Value: "true",
-  //       },
-  //     ],
-  //   },
-  // ];
-
   for (var i = 0; i < users.length; i++) {
     console.log(users[i]);
     var poolData = {
@@ -100,52 +33,6 @@ async function myHandler(event, context, callback) {
     //if user exists and attributes are updated in this file updateUserAttributes is needed to update the attributes
     await cognitolib.updateUserAttributes(attributeData);
   }
-
-// async function createUser(params) {
-//   await new Promise((resolve, reject) => {
-//     COGNITO_CLIENT.adminCreateUser(params, function (err, data) {
-//       if (err) {
-//         console.log("FAILED ", err, err.stack); // an error occurred
-//         response = { statusCode: 500, body: { message: "FAILED", error: err } };
-//         resolve(); //if user already exists, we still continue and ignore
-//       } else {
-//         console.log("SUCCESS", data); // successful response
-//         response = { statusCode: 200, body: { message: "SUCCESS" } };
-//         resolve();
-//       }
-//     });
-//   });
-// }
-//
-// async function setPassword(params) {
-//   await new Promise((resolve, reject) => {
-//     COGNITO_CLIENT.adminSetUserPassword(params, function (err, data) {
-//       if (err) {
-//         console.log("FAILED to update password", err, err.stack); // an error occurred
-//         response = { statusCode: 500, body: { message: "FAILED", error: err } };
-//         reject();
-//       } else {
-//         console.log("SUCCESS", data);
-//         resolve();
-//       }
-//     });
-//   });
-// }
-//
-// async function updateUserAttributes(params) {
-//   await new Promise((resolve, reject) => {
-//     COGNITO_CLIENT.adminUpdateUserAttributes(params, function (err, data) {
-//       if (err) {
-//         console.log("FAILED to update user attributes", err, err.stack); // an error occurred
-//         response = { statusCode: 500, body: { message: "FAILED", error: err } };
-//         reject();
-//       } else {
-//         console.log("SUCCESS", data);
-//         resolve();
-//       }
-//     });
-//   });
-// }
 
 }
 
