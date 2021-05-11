e40c5e596b8c86e1681aa79c920542abdf933edc
Ayo A <ayoneeyee@gmail.com>
2021-05-11 16:10:10 -0400

    Updated code to reject HTML file extension from upload

--- services/ui-src/src/containers/NewAmendment.js
+++ services/ui-src/src/containers/NewAmendment.js
@@ -45,15 +45,13 @@ export default function NewAmendment({ fileUpload }) {
 
     if (!validateFileAttachment(file)) return;
 
-    var validExt = ".html";
+    var validExt = ".png, .gif, .jpeg, .jpg";
      var filePath = event.value;
      var getFileExt = filePath.substring(filePath.lastIndexOf('.') + 1).toLowerCase();
      var pos = validExt.indexOf(getFileExt);
-     if(pos === 0) {
+     if(pos < 0) {
        alert("This file is not allowed, please upload valid file.");
-       return false;
-      } else {
-        return true;
+       return ;
       }
 
     setIsLoading(true);
