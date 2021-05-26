f581da08fd54271e019afe8765faacf2eb80691f
Mike Dial <48921055+mdial89f@users.noreply.github.com>
2021-05-26 16:18:43 -0400

    Add Code Scanning powered by CodeQL (#239)
    * Add a CodeQL analysis workflow
    
    * Dev... adding codeql as a supported branch
    
    * Add branch, but specifying the codeql branch for PR reasons... need to remove branch=codeql prior to merge
    
    * Make a bad change to test codeql
    
    * prettier
    
    * Point badge to master branch
    
    * Remove dev branches
    
    * Revert "Make a bad change to test codeql"
    
    This reverts commit dd9dde9b12c16cf8207e418b41d146dcad250fb9.
    
    * Remove console.log statement per codeql finding
    
    * poitn the readme to master branch
    
    * Run codeql checks against higher environments too, to see diffs

--- services/uploads/src/scan.js
+++ services/uploads/src/scan.js
@@ -3,7 +3,6 @@
 const AWS = require("aws-sdk");
 
 module.exports.main = async (event, context, callback) => {
-  console.log("ENVIRONMENT VARIABLES\n" + JSON.stringify(process.env, null, 2));
   console.info("EVENT\n" + JSON.stringify(event, null, 2));
   console.info("CONTEXT\n" + JSON.stringify(context, null, 2));
   console.info("CALLBACK\n" + JSON.stringify(callback, null, 2));
