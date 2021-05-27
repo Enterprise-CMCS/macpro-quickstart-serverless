0a7ae8b2de22503258cde0312577b23344627965
Mike Dial <michael.dial.4@gmail.com>
2021-05-27 10:55:26 -0400

    Add HSTS lambda

---
+++ services/ui/handlers/hsts.js
@@ -0,0 +1,11 @@
+
+exports.handler = (event, context, callback) => {
+  if (event.source == "serverless-plugin-warmup") {
+    console.log("Warmed up!");
+    return null;
+  }
+  const response = event.Records[0].cf.response;
+  const headers = response.headers;
+  headers['strict-transport-security'] = [{key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubdomains; preload'}];
+  return callback(null, response);
+};
