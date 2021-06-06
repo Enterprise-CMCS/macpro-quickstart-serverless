827690caa68db547aead9ce547f0100a2b5d7945
Mike Dial <michael.dial.4@gmail.com>
2021-06-06 15:07:21 -0400

    export ZIPOPT to finally get this working a-ok on ubuntu.  adding comments and the link

--- libs/plugins/serverless-webpack-idempotent/index.js
+++ libs/plugins/serverless-webpack-idempotent/index.js
@@ -10,15 +10,21 @@ class ServerlessPlugin {
     this.options = options;
 
     this.hooks = {
-      "webpack:compile:compile": this.repackZips.bind(this),
+      "webpack:compile:compile": this.readyFilesForIdempotentZipping.bind(this),
     };
   }
 
-  repackZips() {
-    this.serverless.cli.log("Setting timestamps...");
+  readyFilesForIdempotentZipping() {
+    this.serverless.cli.log("Readying function files for idempotent zipping...");
+
+    // Tell the zip command to forego creating directory entries in the archive, via the ZIPOPT variable.
+    // see:  https://linux.die.net/man/1/zip
+    process.env.ZIPOPT = "-D";
+
     const serviceDir = this.serverless.serviceDir;
     const time = new Date("631152000000");
 
+    // Discover the functions ready to be packed.
     var dirs;
     if (isIndividialPackaging.call(this)) {
       const functionNames = this.options.function
@@ -29,6 +35,8 @@ class ServerlessPlugin {
       dirs = ["service"];
     }
 
+    // Iterate over each function directory, glob all files, and reset atime and mtime
+    // see:  https://medium.com/@pat_wilson/building-deterministic-zip-files-with-built-in-commands-741275116a19
     dirs.forEach((dir) => {
       var dirPath = `${serviceDir}/.webpack/${dir}/**/*`;
       let files = glob.sync(dirPath, {
