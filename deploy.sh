c93a45c59293ab92ed525d6f76b2ef27d2d79032
Mike Dial <michael.dial.4@gmail.com>
2021-06-07 11:44:39 -0400

    Remove debug stuff

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,15 @@ set -e
 stage=${1:-dev}
 
 services=(
-  # 'database'
-  # 'uploads'
+  'database'
+  'uploads'
   'app-api'
-  # 'stream-functions'
-  # 'ui-waflog-s3-bucket'
-  # 'ui'
-  # 'ui-auth'
-  # 'ui-waf-log-assoc'
-  # 'ui-src'
+  'stream-functions'
+  'ui-waflog-s3-bucket'
+  'ui'
+  'ui-auth'
+  'ui-waf-log-assoc'
+  'ui-src'
 )
 
 install_deps() {
@@ -40,16 +40,6 @@ export PATH=$(pwd)/node_modules/.bin/:$PATH
 for i in "${services[@]}"
 do
 	deploy $i
-  ls -la services/app-api/.serverless
-  sha256sum services/app-api/.serverless/create.zip
-  sha256sum services/app-api/.serverless/warmUpPlugin.zip
-  sha256sum services/app-api/.serverless/custom-resources.zip
-  # cat services/app-api/.serverless/cloudformation-template-update-stack.json
-  deploy $i
-  sha256sum services/app-api/.serverless/create.zip
-  sha256sum services/app-api/.serverless/warmUpPlugin.zip
-  sha256sum services/app-api/.serverless/custom-resources.zip
-  # cat services/app-api/.serverless/cloudformation-template-update-stack.json
 done
 
 pushd services
