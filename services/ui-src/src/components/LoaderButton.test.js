d53bf7bafedd1022d9fa1ec83c40b14e0ac2ac00
Benjamin Paige <benjamin.paige@gmail.com>
2021-11-03 17:35:29 -0600

    Refactor LoaderButton test

--- services/ui-src/src/components/LoaderButton.test.js
+++ services/ui-src/src/components/LoaderButton.test.js
@@ -3,14 +3,12 @@ import LoaderButton from "./LoaderButton";
 import { render, within } from "@testing-library/react";
 
 describe("Test LoaderButton.js", () => {
-  test("Check that LoaderButton had correct classNames", () => {
-    const { container } = render(<LoaderButton />);
+  test("Check that LoaderButton had correct classNames and renders correct child text", () => {
+    const { container, getByTestId } = render(
+      <LoaderButton>Submit</LoaderButton>
+    );
     expect(container.firstChild).toHaveClass("d-grid");
     expect(container.firstChild).toHaveClass("mt-4");
-  });
-
-  test("renders child text", () => {
-    const { getByTestId } = render(<LoaderButton>Submit</LoaderButton>);
     const { getByText } = within(getByTestId("loader-button"));
     expect(getByText("Submit")).toBeInTheDocument();
   });
