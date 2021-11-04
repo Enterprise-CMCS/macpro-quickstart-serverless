a868980a34549c59cf0986674b16ca4692eae11e
Benjamin Paige <benjamin.paige@gmail.com>
2021-11-04 17:46:01 -0600

    Update Header imports

--- services/ui-src/src/components/Header.js
+++ services/ui-src/src/components/Header.js
@@ -1,36 +1,32 @@
 import React from "react";
 import { UsaBanner } from "@cmsgov/design-system";
-import * as Bootstrap from "react-bootstrap";
+import { Navbar, Container, NavDropdown, Nav } from "react-bootstrap";
 import { CMSLogo } from "./CMSLogo";
 
 function Header({ handleLogout, isAuthenticated, handleLogin }) {
   return (
     <>
       <UsaBanner />
-      <Bootstrap.Navbar className="nav-bar">
-        <Bootstrap.Container>
-          <Bootstrap.Navbar.Brand href="/">
+      <Navbar className="nav-bar">
+        <Container>
+          <Navbar.Brand href="/">
             <CMSLogo />
-          </Bootstrap.Navbar.Brand>
-          <Bootstrap.Navbar.Toggle />
-          <Bootstrap.Navbar.Collapse className="justify-content-end">
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
             {isAuthenticated ? (
-              <Bootstrap.NavDropdown title="My Account">
-                <Bootstrap.NavDropdown.Item href="/profile">
-                  Profile
-                </Bootstrap.NavDropdown.Item>
-                <Bootstrap.NavDropdown.Item onClick={handleLogout}>
+              <NavDropdown title="My Account">
+                <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
+                <NavDropdown.Item onClick={handleLogout}>
                   Logout
-                </Bootstrap.NavDropdown.Item>
-              </Bootstrap.NavDropdown>
+                </NavDropdown.Item>
+              </NavDropdown>
             ) : (
-              <Bootstrap.Nav.Link onClick={handleLogin}>
-                Login
-              </Bootstrap.Nav.Link>
+              <Nav.Link onClick={handleLogin}>Login</Nav.Link>
             )}
-          </Bootstrap.Navbar.Collapse>
-        </Bootstrap.Container>
-      </Bootstrap.Navbar>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 }
