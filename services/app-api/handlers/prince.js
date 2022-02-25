52891ab6d94a53a536300fec789a5e8f027a9a8e
Berry Davenport <bdavenport@collabralink.com>
2022-02-25 11:54:19 -0500

    Update prince

--- services/app-api/handlers/prince.js
+++ services/app-api/handlers/prince.js
@@ -1,24 +1,30 @@
 import handler from "./../libs/handler-lib";
-var execSync = require("child_process").execSync;
-const fs = require("fs");
+import { execSync } from "child_process";
+import * as fs from "fs";
 
-export const main = handler(async (event, context) => {
+export const prince = async (event, context) => {
   // If this invocation is a prewarm, do nothing and return.
-  if (event.source == "serverless-plugin-warmup") {
+  if (event && event.source == "serverless-plugin-warmup") {
     console.log("Warmed up!");
     return null;
   }
+
   if (!event || !event.body) {
-    return new Error("No data.");
+    throw new Error("No data.");
   }
-  let body = event.body;
-  let html = Buffer.from(body, "base64").toString("ascii");
+
+  const body = event.body;
+  const b64EncodedString = Buffer.from(body, "base64");
+
+  let html = b64EncodedString.toString("ascii");
   fs.writeFileSync("/tmp/input", html);
   try {
-    // ["-", "-o", "-", "--pdf-profile=PDF/UA-1"],
     let result = execSync(`/opt/prince /tmp/input -o - --pdf-profile=PDF/UA-1`);
     return result.toString("base64");
   } catch (err) {
     console.log(err);
+    throw new Error(err);
   }
-});
+};
+
+export const main = handler(prince);
