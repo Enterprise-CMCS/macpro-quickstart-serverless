613b92e2f9ca3aeac144e373792468c637456c23
Nathan Nti <nnti@ip-10-232-46-37.ec2.internal>
2022-03-16 13:28:52 -0400

    modified prince

---
+++ services/app-api/handlers/princeProxy.js
@@ -0,0 +1,27 @@
+const axios =  require('axios');
+
+exports.handler = async (event) => {
+  let response = {};
+  
+  let api_endpoint = event['princeApi'];
+
+  await axios({ method: 'POST',
+      url: api_endpoint,
+      "data":"test",
+      headers:{
+          "Content-Type" : "application/json"
+      }
+  }).then((result) => {
+      console.log(result.data);
+      response = {
+          status: 200,
+          response: result.data
+      }
+  }).catch((error) => {
+      console.log("Error occured: " + error);
+      response = {
+          error 
+      }
+  });
+  return response;
+}
