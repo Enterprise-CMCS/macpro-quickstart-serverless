9f694d0bf8d9e65bbb324cde46ac9584641eec4c
Nathan Nti <nnti@ip-10-232-38-119.ec2.internal>
2022-03-21 16:53:11 -0400

    results

---
+++ services/app-api/handlers/princeProxy.js
@@ -0,0 +1,30 @@
+import handler from "./../libs/handler-lib";
+const axios =  require('axios');
+
+export const princeProxy = async (event) => {
+  let response = {};
+  let api_endpoint = process.env.princeApi;
+
+  console.log("Api string", process.env.princeApi);
+  console.log(event['body']);
+
+  await axios({ method: 'POST',
+      url: api_endpoint,
+      data: event['body'],
+      headers: {
+        "Content-Type" : "application/json"
+      }
+    }).then((result) => {
+      response = {
+        statusCode: 200,
+        response: result.data
+      };
+    }).catch((error) => {
+      response = {
+        error
+    };
+  });
+  return response.response;
+};
+
+export const main = handler(princeProxy);
