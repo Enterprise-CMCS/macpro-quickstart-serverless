e24bb466b86eb90d30e1a80c2bd59ad45036ab54
Nathan Nti <nnti@itadminss-MacBook-Pro.local>
2022-03-25 17:22:16 -0400

    proxy api

--- services/proxy-api/handlers/proxy.js
+++ services/proxy-api/handlers/proxy.js
@@ -3,10 +3,7 @@ const axios = require("axios");
 
 export const proxy = async (event) => {
   let response = {};
-  let api_endpoint = process.env.princeApi;
-
-  // console.log("Api string", process.env.princeApi);
-  // console.log(event['body']);
+  let api_endpoint = process.env.proxyApi;
 
   await axios({
     method: "POST",
