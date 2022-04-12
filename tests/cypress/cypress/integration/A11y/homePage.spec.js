020c3dc266eb7a9d8745ed3c9b18ceb54f4ba24c
aalousi <aalousi@collabralink.com>
2022-04-12 14:37:30 -0400

    renaming folder

--- tests/cypress/cypress/integration/A11y/homePage.spec.js
+++ tests/cypress/cypress/integration/A11y/homePage.spec.js
@@ -7,4 +7,3 @@ describe("Home Page 508 test", () => {
     cy.checkA11yOfPage();
   });
 });
-//Child Core Set Questions Does Not Have a test. Please make the same modifications to that page when making modifications to this page.
