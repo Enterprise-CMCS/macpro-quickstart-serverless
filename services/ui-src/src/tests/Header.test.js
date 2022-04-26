ec628497ccd7594905093d7b360143eb33f4379e
Benjamin Paige <benjamin.paige@gmail.com>
2022-04-26 14:37:14 -0600

    Add matchMedia stub and update snapshot

--- services/ui-src/src/tests/Header.test.js
+++ services/ui-src/src/tests/Header.test.js
@@ -2,6 +2,16 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Header from "../components/Header";
 
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addEventListener: function () {},
+      removeEventListener: function () {},
+    };
+  };
+
 describe("Test Header.js", () => {
   test("Should match snapshot", () => {
     expect(render(<Header />).asFragment()).toMatchSnapshot();
