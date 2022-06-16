69c5e5b49a626ede1203136c2b3ea8aceee533ea
Leslie Klein <leslie@corbalt.com>
2022-06-15 20:47:13 -0400

    update app-api service README

--- services/app-api/handlers/delete.js
+++ services/app-api/handlers/delete.js
@@ -2,7 +2,7 @@ import handler from "./../libs/handler-lib";
 import dynamoDb from "./../libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
-  // If this invokation is a prewarm, do nothing and return.
+  // If this invocation is a pre-warm, do nothing and return.
   if (event.source == "serverless-plugin-warmup") {
     console.log("Warmed up!");
     return null;
