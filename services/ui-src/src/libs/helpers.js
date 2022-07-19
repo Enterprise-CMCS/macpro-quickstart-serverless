de8470e2218bed66c7991edc5f8802ce2bf9c43c
ben-harvey <benjaminriderharvey@gmail.com>
2022-07-19 08:55:40 -0400

    fix command injection vulnerability

--- services/ui-src/src/libs/helpers.js
+++ services/ui-src/src/libs/helpers.js
@@ -24,17 +24,29 @@ export function validateFileAttachment(file) {
       );
       return false;
     }
+
+    var fileName = file.current.name;
+
+    // validate file name
+    const fileNameRegex = new RegExp("^[0-9a-zA-z-_.]*$");
+    if (!fileNameRegex.test(fileName)) {
+      alert(
+        `The file name contains invalid characters. Only the following characters are allowed: A-Z, a-z, 0-9, -, _, and .`
+      );
+      return false;
+    }
+
+    // validate file extension
     const validExt =
       ".bmp,.csv,.doc,.docx,.gif,.jpg,.jpeg,.odp,.ods,.odt,\n.png,.pdf,.ppt,.pptx,.rtf,.tif,.tiff,.txt,.xls,.xlsx";
-    var fileName = file.current.name;
     var fileExt = fileName
       .substring(fileName.lastIndexOf(".") + 1)
       .toLowerCase();
     var pos = validExt.indexOf(fileExt);
     if (pos < 0) {
       alert(
-        `This file type is not allowed.  
-Only files with one of the following extensions are allowed:  
+        `This file type is not allowed.
+Only files with one of the following extensions are allowed:
 ${validExt}`
       );
       return false;
