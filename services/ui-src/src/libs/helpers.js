dd4b9f63e64409a918844ab4c19a2799fe23f64c
ben-harvey <benjaminriderharvey@gmail.com>
2022-07-19 12:47:47 -0400

    refactor helpers

--- services/ui-src/src/libs/helpers.js
+++ services/ui-src/src/libs/helpers.js
@@ -14,43 +14,52 @@ export function validateAmendmentForm(email, firstName, lastName, territory) {
   );
 }
 
+function validFileSize(file) {
+  if (file.current.size > config.MAX_ATTACHMENT_SIZE) {
+    alert(
+      `Please pick a file smaller than ${
+        config.MAX_ATTACHMENT_SIZE / 1000000
+      } MB.`
+    );
+    return false;
+  }
+  return true;
+}
+
+function validFileName(fileName) {
+  const fileNameRegex = new RegExp("^[0-9a-zA-z-_.]*$");
+  if (!fileNameRegex.test(fileName)) {
+    alert(
+      `The file name contains invalid characters. Only the following characters are allowed: A-Z, a-z, 0-9, -, _, and .`
+    );
+    return false;
+  }
+  return true;
+}
+
+function validFileExtension(fileName) {
+  const validExt =
+    ".bmp,.csv,.doc,.docx,.gif,.jpg,.jpeg,.odp,.ods,.odt,\n.png,.pdf,.ppt,.pptx,.rtf,.tif,.tiff,.txt,.xls,.xlsx";
+  var fileExt = fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
+  var pos = validExt.indexOf(fileExt);
+  if (pos < 0) {
+    alert(
+      `This file type is not allowed.
+  Only files with one of the following extensions are allowed:
+  ${validExt}`
+    );
+    return false;
+  }
+  return true;
+}
+
 export function validateFileAttachment(file) {
   if (file.current) {
-    if (file.current.size > config.MAX_ATTACHMENT_SIZE) {
-      alert(
-        `Please pick a file smaller than ${
-          config.MAX_ATTACHMENT_SIZE / 1000000
-        } MB.`
-      );
-      return false;
-    }
-
-    var fileName = file.current.name;
-
-    // validate file name
-    const fileNameRegex = new RegExp("^[0-9a-zA-z-_.]*$");
-    if (!fileNameRegex.test(fileName)) {
-      alert(
-        `The file name contains invalid characters. Only the following characters are allowed: A-Z, a-z, 0-9, -, _, and .`
-      );
-      return false;
-    }
-
-    // validate file extension
-    const validExt =
-      ".bmp,.csv,.doc,.docx,.gif,.jpg,.jpeg,.odp,.ods,.odt,\n.png,.pdf,.ppt,.pptx,.rtf,.tif,.tiff,.txt,.xls,.xlsx";
-    var fileExt = fileName
-      .substring(fileName.lastIndexOf(".") + 1)
-      .toLowerCase();
-    var pos = validExt.indexOf(fileExt);
-    if (pos < 0) {
-      alert(
-        `This file type is not allowed.
-Only files with one of the following extensions are allowed:
-${validExt}`
-      );
-      return false;
-    }
+    return (
+      validFileSize(file) &&
+      validFileName(file.current.name) &&
+      validFileExtension(file.current.name)
+    );
   }
   return true;
 }
