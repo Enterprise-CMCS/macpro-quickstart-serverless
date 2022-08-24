aa7d1aa36a89a7a74a9729f7963f2daa80748b42
ben-harvey <benjaminriderharvey@gmail.com>
2022-08-24 13:04:21 -0400

    First commit

--- services/ui-src/src/App.js
+++ services/ui-src/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useHistory } from "react-router-dom";
 import { Auth } from "aws-amplify";
 import { AppContext } from "./libs/contextLib";
@@ -8,17 +8,91 @@ import Routes from "./Routes";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import config from "./config";
+import { useIdleTimer } from "react-idle-timer";
+import Modal from "react-modal";
+import { Dialog } from "@cmsgov/design-system";
 
-function App() {
+Modal.setAppElement("#root");
+
+function App(props) {
   const [isAuthenticating, setIsAuthenticating] = useState(true);
   const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [remaining, setRemaining] = useState(props.promptTimeout);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
   const history = useHistory();
+
+  const handleOnActive = () => {
+    console.log("Active");
+  };
+
+  let promptInterval;
+  const handleOnPrompt = () => {
+    console.log("Opening Modal");
+    setModalIsOpen(true);
+
+    const tick = () => setRemaining(Math.ceil(getRemainingTime() / 1000));
+
+    clearInterval(promptInterval);
+    promptInterval = setInterval(tick, 1000);
+  };
+
+  const handleOnIdle = () => {
+    clearInterval(promptInterval);
+    setModalIsOpen(false);
+    handleLogout();
+  };
+
+  const stayActive = () => {
+    setModalIsOpen(false);
+    activate();
+  };
+
+  const {
+    start,
+    reset,
+    pause,
+    activate,
+    resume,
+    getRemainingTime,
+    getLastActiveTime,
+    getElapsedTime,
+  } = useIdleTimer({
+    timeout: 5000,
+    promptTimeout: 5000,
+    onActive: handleOnActive,
+    onPrompt: handleOnPrompt,
+    onIdle: handleOnIdle,
+    startManually: true,
+    startOnMount: false,
+    events: [
+      "mousemove",
+      "keydown",
+      "wheel",
+      "DOMMouseScroll",
+      "mousewheel",
+      "mousedown",
+      "touchstart",
+      "touchmove",
+      "MSPointerDown",
+      "MSPointerMove",
+      "visibilitychange",
+    ],
+  });
 
   useEffect(() => {
     onLoad();
+    const intervalID = setInterval(() => {
+      console.log("Remaining time: ", getRemainingTime());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalID);
+    };
   }, []);
 
   async function onLoad() {
+    console.log("Called onLoad");
     try {
       await Auth.currentSession();
       userHasAuthenticated(true);
@@ -35,7 +109,8 @@ function App() {
     await Auth.signOut();
 
     userHasAuthenticated(false);
-
+    reset();
+    pause();
     history.push("/");
   }
 
@@ -57,6 +132,8 @@ function App() {
       } else {
         await Auth.federatedSignIn();
       }
+      console.log("Called start");
+      start();
     } catch (e) {
       onError(e);
     }
@@ -70,6 +147,27 @@ function App() {
           handleLogout={handleLogout}
           handleLogin={handleLogin}
         />
+        {modalIsOpen && (
+          <Dialog
+            onExit={stayActive}
+            underlayClickExits={true}
+            closeIcon={null}
+            closeButtonText={""}
+          >
+            <h2>Session Timeout</h2>
+            <p>
+              Your session is about to expire due to inactivity. You will be
+              automatically logged out in {remaining} seconds.
+            </p>
+            <button
+              className="ds-c-button ds-c-button--solid ds-u-margin-right--1"
+              key="solid"
+              onClick={stayActive}
+            >
+              Stay Signed In
+            </button>
+          </Dialog>
+        )}
         <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
           <Routes />
           <Footer />
