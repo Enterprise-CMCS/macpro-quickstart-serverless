2f40b68a2a6af4ff4789b44f441bbb80aaabdb66
abelzer212 <93547052+abelzer212@users.noreply.github.com>
2022-08-25 12:06:07 -0500

    continue scratch

--- .github/scripts/scratch.sh
+++ .github/scripts/scratch.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
 start=82449735678d3abe75c5dd4652cd3158d9ae5d73
-end=51d5b9ba30e4006f92efa41328e9201e04da6318
+end1file=51d5b9ba30e4006f92efa41328e9201e04da6318
+end4files=0ecd146cfbe9b9a66c864d1b211c87babd9d285d
 
-diffList=$(git diff ${start}...${end} --name-only)
 
-echo "$diffList"
+
+diffList=$(git diff ${start}...${end4files} --name-only)
+serviceList=()
+
+for i in $diffList; do 
+    echo $i
+done
+
