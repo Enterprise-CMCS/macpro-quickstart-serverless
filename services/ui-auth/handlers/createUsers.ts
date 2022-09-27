a7510db906015b43e4f66b72e6085ff4c280b4fe
abelzer212 <93547052+abelzer212@users.noreply.github.com>
2022-09-27 12:28:14 -0500

    commit

---
+++ services/ui-auth/handlers/createUsers.ts
@@ -0,0 +1,46 @@
+import * as cognitolib from "../libs/cognito-lib";
+const userPoolId = process.env.userPoolId;
+const users = require("../libs/users.json");
+import {
+  APIGatewayProxyResult,
+  APIGatewayEvent,
+  APIGatewayEventRequestContextV2,
+} from "aws-lambda";
+
+async function myHandler(
+  event: APIGatewayEvent,
+  context: APIGatewayEventRequestContextV2,
+  callback: APIGatewayProxyResult
+) {
+  console.log("USER POOL ID: ");
+  console.log(userPoolId);
+
+  for (var i = 0; i < users.length; i++) {
+    console.log(users[i]);
+    var poolData = {
+      UserPoolId: userPoolId,
+      Username: users[i].username,
+      DesiredDeliveryMediums: ["EMAIL"],
+      UserAttributes: users[i].attributes,
+    };
+    var passwordData = {
+      Password: process.env.bootstrapUsersPassword,
+      UserPoolId: userPoolId,
+      Username: users[i].username,
+      Permanent: true,
+    };
+    var attributeData = {
+      Username: users[i].username,
+      UserPoolId: userPoolId,
+      UserAttributes: users[i].attributes,
+    };
+
+    await cognitolib.createUser(poolData);
+    //userCreate must set a temp password first, calling setPassword to set the password configured in SSM for consistent dev login
+    await cognitolib.setPassword(passwordData);
+    //if user exists and attributes are updated in this file updateUserAttributes is needed to update the attributes
+    await cognitolib.updateUserAttributes(attributeData);
+  }
+}
+
+exports.handler = myHandler;
